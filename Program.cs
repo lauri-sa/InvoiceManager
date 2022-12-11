4edf9e48a1e753f781b2b34f49fa98dd2e24c904
lauri-sa <guzovovo@gmail.com>
2022-12-11 11:55:14 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Harjoitustyo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Laskutussovellus\n");
+
+            Console.WriteLine("1 = \n");
+
+            Console.WriteLine("2 = \n");
+
+            Console.WriteLine("3 = \n");
+
+            Console.WriteLine("4 = \n");
+
+            Console.WriteLine("5 = \n");
+
+            Console.WriteLine("6 = \n");
+        }
+    }
+}
